2901dba34c868e870f2e94275ac1345e02f5c0d0
marante <bugger@bravis.se>
2018-01-03 22:21:14 +0100

    Added navbar fixes. Links around radioshowimages. Fixed errors with DB connection

--- OpieAndAnthonyRadioShow/Startup.cs
+++ OpieAndAnthonyRadioShow/Startup.cs
@@ -47,7 +47,7 @@ namespace OpieAndAnthonyRadioShow
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=RadioShow}/{action=Index}/{id?}");
             });
         }
     }
